c725cea0f1fc4ba81f178d37f550ddb7d948e9c4
emarco <emarco@google.com>
2022-08-23 13:14:27 +0300

    final touches to FE

--- application/db_helper.py
+++ application/db_helper.py
@@ -4,11 +4,12 @@ import os
 from base64 import b64decode
 
 tasks = [
-    {"id": 1, "task": "implement application", "status": "In Progress"},
-    {"id": 2, "task": "find demo idea", "status": "Complete"},
-    {"id": 3, "task": "Get Approval for video", "status": "Todo"},
-    {"id": 4, "task": "Record Screencast", "status": "Todo"},
-    {"id": 5, "task": "Edit Video", "status": "Todo"},
+    {"id": 1, "task": "Implement python application", "status": "Complete"},
+    {"id": 2, "task": "Write unit tests", "status": "Complete"},
+    {"id": 3, "task": "Build CI/CD pipline", "status": "Complete"},
+    {"id": 4, "task": "Deploy application to Cloud Run", "status": "In Progress"},
+    {"id": 5, "task": "Integrate SAST in Cloud build CI", "status": "Todo"},
+    # {"id": 6, "task": "Optimize BE code ", "status": "Todo"},
 ]
 
 
@@ -17,44 +18,18 @@ def fetch_todo():
 
 
 def update_task_entry(task_id: int, text: str) -> None:
-    """Updates task description based on given `task_id`
-
-    Args:
-        task_id (int): Targeted task_id
-        text (str): Updated description
-
-    Returns:
-        None
-    """
     for task in tasks:
         if task["id"] == task_id:
             task["task"] = text
 
 
 def update_status_entry(task_id: int, text: str) -> None:
-    """Updates task status based on given `task_id`
-
-    Args:
-        task_id (int): Targeted task_id
-        text (str): Updated status
-
-    Returns:
-        None
-    """
     for task in tasks:
         if task["id"] == task_id:
             task["status"] = text
 
 
 def insert_new_task(text: str) -> int:
-    """Insert new task to table.
-
-    Args:
-        text (str): Task
-
-    Returns: The task ID for the inserted entry
-    """
-
     last_task_id = tasks[-1]["id"]
     tasks.append({"id": last_task_id + 1, "task": text, "status": "Todo"})
     try:
