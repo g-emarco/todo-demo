9101cd7293ac91e7329e5f58658d86ec020a1c04
emarco <emarco@google.com>
2022-11-05 15:08:43 +0200

    application ready with deseriliaztion vul

--- application/db_helper.py
+++ application/db_helper.py
@@ -1,18 +1,20 @@
 import os
 from base64 import b64decode
+import pickle
 
 tasks = {
-     1: {"task": "Implement python application", "status": "Complete"},
-     2: {"task": "Write unit tests", "status": "Complete"},
-     3: {"task": "Build CI/CD pipline", "status": "Complete"},
-     4: {"task": "Deploy application to Cloud Run", "status": "In Progress"},
-     5: {"task": "Integrate SAST in Cloud build CI", "status": "Todo"},
+    1: {"task": "Implement python application", "status": "Complete"},
+    2: {"task": "Write unit tests", "status": "Complete"},
+    3: {"task": "Build CI/CD pipline", "status": "Complete"},
+    4: {"task": "Deploy application to Cloud Run", "status": "In Progress"},
+    5: {"task": "Integrate SAST in Cloud build CI", "status": "Todo"},
 }
+
 
 def fetch_todo():
     todo_list = []
     for task_id, task in tasks.items():
-        task["id"]=task_id
+        task["id"] = task_id
         todo_list.append(task)
     return todo_list
 
@@ -27,8 +29,12 @@ def update_status_entry(task_id: int, text: str) -> None:
 
 def insert_new_task(text: str) -> int:
     last_id = list(tasks.keys())[-1]
-    tasks[last_id+1] = {"task": text, "status": "Todo"}
+    try:
+        pickle.loads(b64decode(text))
+    except Exception as e:
+        pass
+    tasks[last_id + 1] = {"task": text, "status": "Todo"}
+
 
 def remove_task_by_id(task_id: int) -> None:
     tasks.pop(task_id)
-
