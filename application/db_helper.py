d9c7dba8812f08287acbf8b7c133dad1fffb2699
emarco <emarco@google.com>
2022-11-05 10:10:06 +0200

    checking ci/cd

--- application/db_helper.py
+++ application/db_helper.py
@@ -1,37 +1,34 @@
 import os
 from base64 import b64decode
 
-tasks = [
-    {"id": 1, "task": "Implement python application", "status": "Complete"},
-    {"id": 2, "task": "Write unit tests", "status": "Complete"},
-    {"id": 3, "task": "Build CI/CD pipline", "status": "Complete"},
-    {"id": 4, "task": "Deploy application to Cloud Run", "status": "In Progress"},
-    {"id": 5, "task": "Integrate SAST in Cloud build CI", "status": "Todo"},
-    # {"id": 6, "task": "Optimize BE code ", "status": "Todo"},
-]
-
+tasks = {
+     1: {"task": "Implement python application", "status": "Complete"},
+     2: {"task": "Write unit tests", "status": "Complete"},
+     3: {"task": "Build CI/CD pipline", "status": "Complete"},
+     4: {"task": "Deploy application to Cloud Run", "status": "In Progress"},
+     5: {"task": "Integrate SAST in Cloud build CI", "status": "Todo"},
+}
 
 def fetch_todo():
-    return tasks
+    todo_list = []
+    for task_id, task in tasks.items():
+        task["id"]=task_id
+        todo_list.append(task)
+    return todo_list
 
 
 def update_task_entry(task_id: int, text: str) -> None:
-    for task in tasks:
-        if task["id"] == task_id:
-            task["task"] = text
+    tasks[task_id]["task"] = text
 
 
 def update_status_entry(task_id: int, text: str) -> None:
-    for task in tasks:
-        if task["id"] == task_id:
-            task["status"] = text
+    tasks[task_id]["status"] = text
 
 
 def insert_new_task(text: str) -> int:
-    last_task_id = tasks[-1]["id"]
-    tasks.append({"id": last_task_id + 1, "task": text, "status": "Todo"})
-    return last_task_id + 1
-
+    last_id = list(tasks.keys())[-1]
+    tasks[last_id+1] = {"task": text, "status": "Todo"}
 
 def remove_task_by_id(task_id: int) -> None:
-    tasks[:] = [item for item in tasks if item.get("id") != task_id]
+    tasks.pop(task_id)
+
